1ee7251fbb54f8302dc3cbaa2b005f38b4267916
Enrique Marina <eherraiz@myuax.com>
2022-05-10 11:30:37 +0200

    seguimos probando

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from prueba import *
+from reparto_observaciones import RepartoObservaciones
+import pandas as pd
+from math import sqrt, ceil
 
 if __name__ == '__main__':
     df = pd.read_csv('criticas.csv')
+    df = RepartoObservaciones(df)
+    cantidad_de_observaciones()
