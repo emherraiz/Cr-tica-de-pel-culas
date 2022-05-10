12b333595d202e0370dbff22778e4e31d126fb40
Enrique Marina <eherraiz@myuax.com>
2022-05-10 08:29:47 +0200

    tenemos media y desviacion

--- prueba.py
+++ prueba.py
@@ -1,10 +1,24 @@
 import pandas as pd
+from math import sqrt
 
 df = pd.read_csv('criticas.csv', sep = ',' )
 x = 0
 for i in range(len(df)):
     x += df.iloc[i][0] * df.iloc[i][1]
 
-media = x / sum(df.iloc[:][1])
+media = x / df['Cantidad'].sum()
 
 print(media)
+x = 0
+for i in range(len(df)):
+    x += df.iloc[i][1] *  pow((df.iloc[i][0] - media), 2)
+
+varianza = x / df['Cantidad'].sum()
+desviacion = sqrt(varianza)
+
+
+print(varianza)
+print(desviacion)
+
+
+
